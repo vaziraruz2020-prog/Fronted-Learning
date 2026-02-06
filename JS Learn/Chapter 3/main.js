575d9a353027b80b38b1f5ac446279e4233b37dc
SUHARIK <vaziraruz2020@gmail.com>
2026-02-07 00:29:11 +0500

    cxxc

--- JS Learn/Chapter 3/main.js
+++ JS Learn/Chapter 3/main.js
@@ -59,9 +59,62 @@ for (const element of y) {
     console.log(Math.sqrt(element ** 6))
 }
 
-const huy = (item, index) => {
-    console.log(`${item} => ${index}`)
-
-}
+const huy = (item, index) => console.log(`${item} => ${index}`)
 
 y.forEach(huy)
+
+let x = {
+    name: 'Kolya',
+    age: 13,
+    job: 'WebDevelopment'
+}
+
+console.log(x)
+console.log(x.age)
+console.log(x['name'])
+
+const huyy = 'job'
+x.age = 14
+x.isMarried = false
+x.fnc = (func, t) => {
+    let reult = func(t);
+    console.log(reult);
+}
+
+console.log(x[huyy])
+console.log(x)
+x.fnc(Arr, y)
+
+// for (let key of x) {
+//     console.log(key)
+// } for_of is for iterable stuff
+
+for (let key in x) {
+    console.log(key)
+    console.log(key, ':', x[key]);
+}
+// for_in is for not iterable stuff
+
+let xy = { ...x }
+xy.age = 1
+
+console.log(xy)
+console.log(x)
+
+class Person {
+    constructor(name, age, job, isMarried, fnc) {
+        this.name = name;
+        this.age = age;
+        this.job = job;
+        this.isMarried = isMarried;
+    }
+     fnc(func, t) {
+        let reult = func(t);
+        console.log(reult);
+    }
+}
+
+const person = new Person('Амир', 13,'student', false );
+
+console.log(person);
+person.fnc(Arr, y)
